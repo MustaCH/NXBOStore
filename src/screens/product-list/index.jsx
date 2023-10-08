b57936c3d31d6eeaad2fc55002e62b31fa13874e
Musta <polettiignacio7@gmail.com>
2023-10-08 09:12:32 -0300

    [feature] - modified img link at product-list

--- src/screens/product-list/index.jsx
+++ src/screens/product-list/index.jsx
@@ -7,81 +7,81 @@ function ProductList() {
     <div className="lg:pl-28">
       <Header />
 
-      <div className="mb-24 lg:my-24 flex flex-col lg:grid lg:grid-cols-2 items-center uppercase text-gray-300 font-semibold text-4xl lg:text-8xl ">
+      <div className="mb-24 lg:my-24 flex flex-col gap-4 lg:grid lg:grid-cols-2 items-center uppercase text-gray-300 font-semibold text-4xl lg:text-8xl ">
         <Link
           to={"/cat/shirts"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group"
+          className="my-4 lg:my-0 relative flex justify-center transition-all drop-shadow-xl hover:text-orange-600 group"
         >
           <p className="absolute z-40 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             shirts
           </p>
           <img
-            src="img/cat-banner/shirt-cat.jpg"
+            src="https://i.ibb.co/QKthhjn/shirt-cat.jpg"
             alt="categorie-header"
             className="brightness-50"
           />
         </Link>
         <Link
           to={"/cat/hoodies"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group"
+          className="my-4 lg:my-0 relative flex justify-center transition-all drop-shadow-xl hover:text-orange-600 group"
         >
           <p className="absolute z-40 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             hoodies
           </p>
           <img
-            src="img/cat-banner/hoodie-cat.jpg"
+            src="https://i.ibb.co/wz5HMJs/hoodie-cat.jpg"
             alt="categorie-header"
             className="brightness-50 "
           />
         </Link>
         <Link
           to={"/cat/coats"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group"
+          className="my-4 lg:my-0 relative flex justify-center  transition-all drop-shadow-xl hover:text-orange-600 group"
         >
           <p className="absolute z-40 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             coats
           </p>
           <img
-            src="img/cat-banner/coat-cat.jpg"
+            src="https://i.ibb.co/ZxYTy46/coat-cat.jpg"
             alt="categorie-header"
             className="brightness-50"
           />
         </Link>
         <Link
           to={"/cat/pants"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group"
+          className="my-4 lg:my-0 relative flex justify-center transition-all drop-shadow-xl hover:text-orange-600 group"
         >
           <p className="absolute z-40 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             pants
           </p>
           <img
-            src="img/cat-banner/pant-cat.jpg"
+            src="https://i.ibb.co/qRyXsnz/pant-cat.jpg"
             alt="categorie-header"
             className="brightness-50"
           />
         </Link>
         <Link
           to={"/cat/shorts"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group"
+          className="my-4 lg:my-0 relative flex justify-center transition-all drop-shadow-xl hover:text-orange-600 group"
         >
           <p className="absolute z-40 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             shorts
           </p>
           <img
-            src="img/cat-banner/short-cat.jpg"
+            src="https://i.ibb.co/gSMkWkB/short-cat.jpg"
             alt="categorie-header"
             className="brightness-50"
           />
         </Link>
         <Link
           to={"/cat/accesories"}
-          className="my-4 lg:my-0 relative transition-all drop-shadow-xl hover:text-orange-600 group "
+          className="my-4 lg:my-0 relative flex justify-center transition-all drop-shadow-xl hover:text-orange-600 group "
         >
           <p className="absolute z-10 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] group-hover:scale-105 duration-300">
             accesories
           </p>
           <img
-            src="img/cat-banner/accesorie-cat.jpg"
+            src="https://i.ibb.co/j8vpxQh/accesorie-cat.jpg"
             alt="categorie-header"
             className="brightness-50"
           />
